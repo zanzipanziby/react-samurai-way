14c59d6f552b557b0652de5db523cbf8e3b5cb62
ZanziPanziBY <zanzipanziby@gmail.com>
2022-09-02 13:41:14 +0300

    new css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <h1>Hello, samurai! Let's go!</h1>
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
     </div>
   );
 }
